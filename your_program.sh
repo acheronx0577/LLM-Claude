8a6f59ff8b543057664cb54191f142c02085bab4
AcheronX. <richardhuynh53@gmail.com>
2026-04-16 01:00:26 -0700

    fix: guard agent loop against infinite tool recursion

---
+++ your_program.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Use this script to run your program LOCALLY.
+#
+# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
+#
+# Learn more: https://codecrafters.io/program-interface
+
