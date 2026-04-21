e76427d07db9658c6fb8fa6e4733abda4c81fd45
AcheronX. <richardhuynh53@gmail.com>
2026-04-20 17:38:58 -0700

    feat: define Bash tool schema for shell commands

---
+++ app/main.ts
@@ -0,0 +1,8 @@
+import { runAcpServer } from "./acp.ts";
+import { runAgent } from "./agent.ts";
+import { runInteractiveChat, parseArgs, shouldUseTuiChat } from "./chat.ts";
+import { loadApiConfig } from "./config.ts";
+import { runInteractiveTuiChat } from "./tuiChat.ts";
+
+async function main() {
+  const args = process.argv.slice(2);
